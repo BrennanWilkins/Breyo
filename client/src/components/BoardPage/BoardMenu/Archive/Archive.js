c88978731b37dc364b3e3948d6b2e6ad9278e191
Brennan Wilkins <bwilkins767@gmail.com>
2020-11-14 15:48:24 -0600

    Added delete modal for deleting list/board

--- client/src/components/BoardPage/BoardMenu/Archive/Archive.js
+++ client/src/components/BoardPage/BoardMenu/Archive/Archive.js
@@ -4,9 +4,11 @@ import { ActionBtn } from '../../../UI/Buttons/Buttons';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { setCardDetailsArchived, recoverCard, deleteCard, recoverList, deleteList } from '../../../../store/actions';
+import DeleteModal from '../DeleteModal/DeleteModal';
 
 const Archive = props => {
   const [showCards, setShowCards] = useState(true);
+  const [showDeleteID, setShowDeleteID] = useState(null);
 
   return (
     <>
@@ -17,18 +19,22 @@ const Archive = props => {
       {showCards ? props.archivedCards.map(card => (
         <div className={classes.CardContainer} key={card.cardID}>
           <div className={classes.Card} onClick={() => props.setCardDetailsArchived(card.cardID, card.listID, card)}><div>{card.title}</div></div>
-          <div className={classes.Options}>
-            <span onClick={() => props.recoverCard(card.cardID, card.listID, props.boardID)}>Recover Card</span>
-            <span onClick={() => props.deleteCard(card.cardID, card.listID, props.boardID)}>Delete</span>
+          <div>
+            <span className={classes.Option} onClick={() => props.recoverCard(card.cardID, card.listID, props.boardID)}>Recover Card</span>
+            <span className={classes.Option} onClick={() => props.deleteCard(card.cardID, card.listID, props.boardID)}>Delete</span>
           </div>
         </div>
       )) : props.archivedLists.map(list => (
-        <div className={classes.ListContainer} key={list.listID}>
-          <div className={classes.ListTitle}>{list.title}</div>
-          <div className={classes.Options}>
-            <span onClick={() => props.recoverList(list.listID, props.boardID)}>Recover List</span>
-            <span onClick={() => props.deleteList(list.listID, props.boardID)}>Delete</span>
+        <div className={classes.ListOuterContainer} key={list.listID}>
+          <div className={classes.ListContainer}>
+            <div className={classes.ListTitle}>{list.title}</div>
+            <div>
+              <span className={classes.Option} onClick={() => props.recoverList(list.listID, props.boardID)}>Recover List</span>
+              <span className={classes.Option} onClick={() => setShowDeleteID(list.listID)}>Delete</span>
+            </div>
           </div>
+          {showDeleteID === list.listID && <DeleteModal confirmText="DELETE THIS LIST" close={() => setShowDeleteID(null)}
+          delete={() => props.deleteList(list.listID, props.boardID)} userIsAdmin={props.userIsAdmin} mode="list" />}
         </div>
       ))}
     </>
@@ -43,13 +49,15 @@ Archive.propTypes = {
   deleteCard: PropTypes.func.isRequired,
   recoverList: PropTypes.func.isRequired,
   deleteList: PropTypes.func.isRequired,
-  archivedLists: PropTypes.array.isRequired
+  archivedLists: PropTypes.array.isRequired,
+  userIsAdmin: PropTypes.bool.isRequired
 };
 
 const mapStateToProps = state => ({
   archivedCards: state.lists.archivedCards,
   archivedLists: state.lists.archivedLists,
-  boardID: state.board.boardID
+  boardID: state.board.boardID,
+  userIsAdmin: state.board.userIsAdmin
 });
 
 const mapDispatchToProps = dispatch => ({
---
+++ client/src/components/BoardPage/BoardMenu/DeleteModal/DeleteModal.js
@@ -0,0 +1,37 @@
+import React, { useState, useRef } from 'react';
+import classes from './DeleteModal.module.css';
+import PropTypes from 'prop-types';
+import { useModalToggle } from '../../../../utils/customHooks';
+import { CloseBtn, ActionBtn } from '../../../UI/Buttons/Buttons';
+
+const DeleteModal = props => {
+  const modalRef = useRef();
+  useModalToggle(true, modalRef, props.close);
+  const [confirmInput, setConfirmInput] = useState('');
+
+  const deleteHandler = () => {
+    if (confirmInput !== props.confirmText) { return; }
+    props.delete();
+  };
+
+  return (
+    <div ref={modalRef} className={classes.DeleteModal}>
+      <span className={classes.CloseBtn}><CloseBtn close={props.close} /></span>
+      {props.userIsAdmin ? <>
+      <div>Are you sure you want to delete this {props.mode}? This action cannot be undone. Type "{props.confirmText}" below to confirm.</div>
+      <input className={classes.Input} value={confirmInput} onChange={e => setConfirmInput(e.target.value)} />
+      <span className={classes.DeleteBtn}><ActionBtn clicked={deleteHandler}>DELETE</ActionBtn></span></>
+      : <div>You must be an admin to delete this {props.mode}.</div>}
+    </div>
+  );
+};
+
+DeleteModal.propTypes = {
+  close: PropTypes.func.isRequired,
+  delete: PropTypes.func.isRequired,
+  mode: PropTypes.string.isRequired,
+  userIsAdmin: PropTypes.bool.isRequired,
+  confirmText: PropTypes.string.isRequired
+};
+
+export default DeleteModal;
