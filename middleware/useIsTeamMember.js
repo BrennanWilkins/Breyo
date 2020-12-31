3b58567099ed3df567e3e07acf172734d0408dd3
Brennan Wilkins <bwilkins767@gmail.com>
2020-12-31 13:11:35 -0600

    Bug fixes

--- middleware/useIsTeamMember.js
+++ middleware/useIsTeamMember.js
@@ -1,7 +1,7 @@
 // verify user is member of a team
 const useIsTeamMember = (req, res, next) => {
   // teamID may be sent in body or in params
-  const isTeamMember = req.userTeam[req.params.teamID] || req.userTeams[req.body.teamID];
+  const isTeamMember = req.userTeams[req.params.teamID] || req.userTeams[req.body.teamID];
   if (!isTeamMember) { res.status(401).json({ msg: 'MUST BE TEAM MEMBER' }); }
   else { next(); }
 };
