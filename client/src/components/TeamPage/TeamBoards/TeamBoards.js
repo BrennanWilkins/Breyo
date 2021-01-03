e8277d5cf92fa74e027b9af1d520c9aab526be12
Brennan Wilkins <bwilkins767@gmail.com>
2021-01-03 12:28:17 -0600

    Added boards to team page

---
+++ client/src/components/TeamPage/TeamBoards/TeamBoards.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import classes from './TeamBoards.module.css';
+import PropTypes from 'prop-types';
+import BoardList from '../../DashboardPage/BoardList/BoardList';
+import { connect } from 'react-redux';
+
+const TeamBoards = props => {
+  return (
+    <div className={classes.Container}>
+      <BoardList boards={props.boards.filter(board => board.teamID === props.teamID)} teamID={props.teamID} teamTitle={props.teamTitle} />
+    </div>
+  );
+};
+
+TeamBoards.propTypes = {
+  boards: PropTypes.array.isRequired,
+  teamID: PropTypes.string.isRequired,
+  teamTitle: PropTypes.string.isRequired
+};
+
+const mapStateToProps = state => ({
+  boards: state.auth.boards,
+  teamID: state.team.teamID,
+  teamTitle: state.team.title
+});
+
+export default connect(mapStateToProps)(TeamBoards);
--- client/src/components/TeamPage/TeamPage.js
+++ client/src/components/TeamPage/TeamPage.js
@@ -5,6 +5,7 @@ import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import TeamHeader from './TeamHeader/TeamHeader';
 import { getActiveTeam } from '../../store/actions';
+import TeamBoards from './TeamBoards/TeamBoards';
 
 const TeamPage = props => {
   const [mode, setMode] = useState('boards');
@@ -29,6 +30,7 @@ const TeamPage = props => {
         <Link to={`/team/${props.match.params.url}/?view=members`} className={mode === 'members' ? classes.Active : classes.Btn}>Members</Link>
         <Link to={`/team/${props.match.params.url}/?view=settings`} className={mode === 'settings' ? classes.Active : classes.Btn}>Settings</Link>
       </div>
+      {mode === 'boards' ? <TeamBoards /> : null}
     </div>
   );
 };
