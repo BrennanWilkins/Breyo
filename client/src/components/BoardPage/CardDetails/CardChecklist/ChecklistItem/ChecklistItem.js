f79a4c53f32cb911bee4af98f93a03e3f06b02ad
Brennan Wilkins <bwilkins767@gmail.com>
2021-01-07 22:13:22 -0600

    Modularized checklist item

--- client/src/components/BoardPage/CardDetails/CardChecklist/ChecklistItem/ChecklistItem.js
+++ client/src/components/BoardPage/CardDetails/CardChecklist/ChecklistItem/ChecklistItem.js
@@ -1,32 +1,14 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import classes from './ChecklistItem.module.css';
 import { Checkbox } from '../../../../UI/Inputs/Inputs';
 import PropTypes from 'prop-types';
 import { CloseBtn } from '../../../../UI/Buttons/Buttons';
 import { editIcon } from '../../../../UI/icons';
-import SubmitBtns from '../../../../UI/SubmitBtns/SubmitBtns';
-import TextArea from 'react-textarea-autosize';
-import { useModalToggle } from '../../../../../utils/customHooks';
 import { Draggable } from 'react-beautiful-dnd';
+import EditChecklistItem from './EditChecklistItem';
 
 const ChecklistItem = props => {
   const [showEdit, setShowEdit] = useState(false);
-  const [itemTitle, setItemTitle] = useState(props.title);
-  const formRef = useRef();
-
-  const closeHandler = () => {
-    setShowEdit(false);
-    setItemTitle(props.title);
-  };
-
-  useModalToggle(showEdit, formRef, closeHandler);
-
-  const editHandler = e => {
-    e.preventDefault();
-    if (itemTitle === '' || itemTitle.length > 300) { return setItemTitle(props.title); }
-    props.editItem(itemTitle);
-    setShowEdit(false);
-  };
 
   const getDragStyle = (isDragging, otherStyles) => {
     if (isDragging) { return { background: 'rgb(235, 235, 235)', ...otherStyles }; }
@@ -45,13 +27,7 @@ const ChecklistItem = props => {
             <span className={classes.EditBtn} onClick={() => setShowEdit(true)}>{editIcon}</span>
             <span className={classes.CloseBtn}><CloseBtn close={props.deleteItem} /></span>
           </div></> :
-          <div className={classes.EditItem}>
-            <form onSubmit={editHandler} ref={formRef}>
-              <TextArea maxRows="5" value={itemTitle} onChange={e => setItemTitle(e.target.value)} className={classes.Input}
-              onKeyPress={e => { if (e.key === 'Enter') { editHandler(e); }}} autoFocus />
-              <SubmitBtns close={closeHandler} text="Edit" />
-            </form>
-          </div>}
+          <EditChecklistItem close={() => setShowEdit(false)} editItem={props.editItem} title={props.title} />}
         </div>
       )}
     </Draggable>
---
+++ client/src/components/BoardPage/CardDetails/CardChecklist/ChecklistItem/EditChecklistItem.js
@@ -0,0 +1,41 @@
+import React, { useState, useRef } from 'react';
+import classes from './ChecklistItem.module.css';
+import { useModalToggle } from '../../../../../utils/customHooks';
+import TextArea from 'react-textarea-autosize';
+import PropTypes from 'prop-types';
+import SubmitBtns from '../../../../UI/SubmitBtns/SubmitBtns';
+
+const EditChecklistItem = props => {
+  const [itemTitle, setItemTitle] = useState(props.title);
+  const formRef = useRef();
+  useModalToggle(true, formRef, props.close);
+
+  const keyPressHandler = e => {
+    if (e.key === 'Enter') { editHandler(e); }
+  };
+
+  const editHandler = e => {
+    e.preventDefault();
+    if (!itemTitle || itemTitle.length > 300) { return setItemTitle(props.title); }
+    props.editItem(itemTitle);
+    props.close();
+  };
+
+  return (
+    <div className={classes.EditItem}>
+      <form onSubmit={editHandler} ref={formRef}>
+        <TextArea maxRows="5" value={itemTitle} onChange={e => setItemTitle(e.target.value)} className={classes.Input}
+        onKeyPress={keyPressHandler} autoFocus onFocus={e => e.target.select()} />
+        <SubmitBtns close={props.close} text="Edit" />
+      </form>
+    </div>
+  );
+};
+
+EditChecklistItem.propTypes = {
+  close: PropTypes.func.isRequired,
+  editItem: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired
+};
+
+export default EditChecklistItem;
