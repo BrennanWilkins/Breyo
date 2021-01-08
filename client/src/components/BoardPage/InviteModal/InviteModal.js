595f07bbe63495e3e56ad63ed1ee078918ff57bf
Brennan Wilkins <bwilkins767@gmail.com>
2021-01-07 21:49:06 -0600

    Added email validation to invite modal

--- client/src/components/BoardPage/InviteModal/InviteModal.js
+++ client/src/components/BoardPage/InviteModal/InviteModal.js
@@ -7,26 +7,35 @@ import { connect } from 'react-redux';
 import { sendInvite } from '../../../store/actions';
 import ModalTitle from '../../UI/ModalTitle/ModalTitle';
 import { Input } from '../../UI/Inputs/Inputs';
+import { isEmail } from '../../../utils/authValidation';
 
 const InviteModal = props => {
   const [email, setEmail] = useState('');
   const modalRef = useRef();
+  const [emailInvalid, setEmailInvalid] = useState(false);
   useModalToggle(true, modalRef, props.close);
   useModalPos(true, modalRef);
 
   const inviteHandler = () => {
+    if (!isEmail(email)) { return setEmailInvalid(true); }
     props.sendInvite(email, props.boardID);
     props.close();
+  };
+
+  const emailHandler = e => {
+    setEmail(e.target.value);
+    setEmailInvalid(false);
   };
 
   return (
     <div className={classes.Container} ref={modalRef}>
       <ModalTitle close={props.close} title="Invite to board" light />
       <div className={classes.Input}>
-        <Input value={email} onChange={e => setEmail(e.target.value)} placeholder="Enter user's email" />
+        <Input value={email} onChange={emailHandler} placeholder="Enter user's email" />
         <Button disabled={!email || !props.isAdmin} clicked={inviteHandler}>Send Invite</Button>
       </div>
       {!props.isAdmin && <div className={classes.ErrMsg}>You must be an admin of this board to invite other members.</div>}
+      {emailInvalid && <div className={classes.ErrMsg}>Please enter a valid email address.</div>}
     </div>
   );
 };
