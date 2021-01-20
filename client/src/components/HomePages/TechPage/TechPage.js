f1959b673a50d831627c1017422a0ee852d09b14
Brennan Wilkins <bwilkins767@gmail.com>
2021-01-19 20:33:06 -0600

    Replaced tech imgs with svgs

--- client/src/components/HomePages/TechPage/TechPage.js
+++ client/src/components/HomePages/TechPage/TechPage.js
@@ -2,14 +2,14 @@ import React from 'react';
 import classes from './TechPage.module.css';
 import { sourceCode } from '../../UI/illustrations';
 import HomeNav from '../Navs/HomeNav/HomeNav';
-import * as Imgs from './TechAssets/imgs';
+import * as icons from './techIcons';
 
 const TechPage = () => (
   <div>
     <HomeNav tech />
     <div className={classes.Illustration}>{sourceCode}</div>
     <div className={classes.Section}>
-      <div className={classes.Img}>{Imgs.ReactImg}</div>
+      <div className={classes.Icon}>{icons.reactIcon}</div>
       <div className={classes.Info}>
         <h2>React</h2>
         <p>The front end code is written in React. Due to the single page nature of React, React router is used to achieve multi-page functionality.
@@ -17,21 +17,21 @@ const TechPage = () => (
       </div>
     </div>
     <div className={classes.Section}>
-      <div className={classes.Img}>{Imgs.ReduxImg}</div>
+      <div className={classes.Icon}>{icons.reduxIcon}</div>
       <div className={classes.Info}>
         <h2>Redux</h2>
         <p>Redux is used as a central state container for the React app. All of the data fetching and state updates are handled in redux actions.</p>
       </div>
     </div>
     <div className={classes.Section}>
-      <div className={classes.Img}>{Imgs.NodeJSImg}</div>
+      <div className={classes.Icon}>{icons.nodeJSIcon}</div>
       <div className={classes.Info}>
         <h2>Node.js</h2>
         <p>Node.js along with the Express.js framework is used for the backend REST API.</p>
       </div>
     </div>
     <div className={classes.Section}>
-      <div className={classes.Img}>{Imgs.SocketIOImg}</div>
+      <div className={classes.Icon}>{icons.socketIOIcon}</div>
       <div className={classes.Info}>
         <h2>Socket.IO</h2>
         <p>Socket.IO is used to ensure all users' boards stay in sync with other users and devices.
@@ -39,7 +39,7 @@ const TechPage = () => (
       </div>
     </div>
     <div className={classes.Section}>
-      <div className={classes.Img}>{Imgs.MongoDBImg}</div>
+      <div className={classes.Icon}>{icons.mongoDBIcon}</div>
       <div className={classes.Info}>
         <h2>MongoDB</h2>
         <p>The NoSQL database MongoDB is used for the database along with mongoose for object data modeling.</p>
