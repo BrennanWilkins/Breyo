d9a7663d6184fbcb6764481e1bbab0d6636b63da
Brennan Wilkins <bwilkins767@gmail.com>
2021-01-25 15:11:31 -0600

    Removed react transition group

--- client/src/components/Notifications/Notifications.js
+++ client/src/components/Notifications/Notifications.js
@@ -3,22 +3,19 @@ import PropTypes from 'prop-types';
 import classes from './Notifications.module.css';
 import { connect } from 'react-redux';
 import { CloseBtn } from '../UI/Buttons/Buttons';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { deleteNotif } from '../../store/actions';
 import { alertIcon } from '../UI/icons';
 
 const Notifications = props => (
-  <TransitionGroup className={classes.Notifs}>
-    {props.notifs.map(({ id, msg }, i) => (
-      <CSSTransition key={id} timeout={500} classNames={{ enter: classes.Enter, enterActive: classes.EnterActive, exit: classes.Exit, exitActive: classes.ExitActive }}>
-        <div className={classes.Notif}>
-          <span className={classes.Icon}>{alertIcon}</span>
-          {msg}
-          <CloseBtn close={() => props.deleteNotif(id)} />
-        </div>
-      </CSSTransition>
+  <div className={classes.Notifs}>
+    {props.notifs.map(({ id, msg }) => (
+      <div key={id} className={classes.Notif}>
+        <span className={classes.Icon}>{alertIcon}</span>
+        {msg}
+        <CloseBtn close={() => props.deleteNotif(id)} />
+      </div>
     ))}
-  </TransitionGroup>
+  </div>
 );
 
 Notifications.propTypes = {
