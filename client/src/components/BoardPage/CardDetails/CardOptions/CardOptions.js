c2d0f48b42216beda90a9d1bf6bf9adf9580c9a7
Brennan Wilkins <bwilkins767@gmail.com>
2021-01-28 14:17:00 -0600

    refactored card options

--- client/src/components/BoardPage/CardDetails/CardOptions/CardOptions.js
+++ client/src/components/BoardPage/CardDetails/CardOptions/CardOptions.js
@@ -14,42 +14,37 @@ import ManualMoveModal from '../ManualMoveModal/ManualMoveModal';
 import CustomFieldModal from '../CustomFieldModal/CustomFieldModal';
 
 const CardOptions = props => {
-  const [showLabelModal, setShowLabelModal] = useState(false);
-  const [showDueDateModal, setShowDueDateModal] = useState(false);
-  const [showChecklistModal, setShowChecklistModal] = useState(false);
-  const [showCopyModal, setShowCopyModal] = useState(false);
-  const [showAddMemberModal, setShowAddMemberModal] = useState(false);
-  const [showRoadmapLabelModal, setShowRoadmapLabelModal] = useState(false);
-  const [showManualMoveModal, setShowManualMoveModal] = useState(false);
-  const [showCustomFieldModal, setShowCustomFieldModal] = useState(false);
+  const [shownModal, setShownModal] = useState(null);
+
+  const closeHandler = () => setShownModal(null);
 
   return (
     <div className={classes.Container}>
       <div className={classes.Title}>ADD TO CARD</div>
       <span className={classes.BtnContainer}>
-        <ActionBtn className={classes.Btn} clicked={() => setShowLabelModal(true)}>{labelIcon}Labels</ActionBtn>
-        {showLabelModal && <LabelModal close={() => setShowLabelModal(false)} />}
-        <ActionBtn className={classes.Btn} clicked={() => setShowChecklistModal(true)}>{checklistIcon}Checklist</ActionBtn>
-        {showChecklistModal && <ChecklistModal close={() => setShowChecklistModal(false)} />}
-        <ActionBtn className={classes.Btn} clicked={() => setShowAddMemberModal(true)}>{personIcon}Member</ActionBtn>
-        {showAddMemberModal && <AddCardMember close={() => setShowAddMemberModal(false)} />}
-        <ActionBtn className={classes.Btn} clicked={() => setShowDueDateModal(true)}>{clockIcon}Due Date</ActionBtn>
-        {showCustomFieldModal && <CustomFieldModal close={() => setShowCustomFieldModal(false)} />}
-        <ActionBtn className={`${classes.Btn} ${classes.CustomFieldBtn}`} clicked={() => setShowCustomFieldModal(true)}>{customFieldIcon}Custom Field</ActionBtn>
+        <ActionBtn className={classes.Btn} clicked={() => setShownModal('label')}>{labelIcon}Labels</ActionBtn>
+        {shownModal === 'label' && <LabelModal close={closeHandler} />}
+        <ActionBtn className={classes.Btn} clicked={() => setShownModal('checklist')}>{checklistIcon}Checklist</ActionBtn>
+        {shownModal === 'checklist' && <ChecklistModal close={closeHandler} />}
+        <ActionBtn className={classes.Btn} clicked={() => setShownModal('member')}>{personIcon}Member</ActionBtn>
+        {shownModal === 'member' && <AddCardMember close={closeHandler} />}
+        <ActionBtn className={classes.Btn} clicked={() => setShownModal('date')}>{clockIcon}Due Date</ActionBtn>
+        {shownModal === 'custom' && <CustomFieldModal close={closeHandler} />}
+        <ActionBtn className={`${classes.Btn} ${classes.CustomFieldBtn}`} clicked={() => setShownModal('custom')}>{customFieldIcon}Custom Field</ActionBtn>
         <span className={classes.RoadmapBtnContainer}>
-          <ActionBtn className={`${classes.Btn} ${classes.RoadmapBtn}`} clicked={() => setShowRoadmapLabelModal(true)}>{roadmapIcon}Roadmap Label</ActionBtn>
-          {showRoadmapLabelModal && <RoadmapLabelModal close={() => setShowRoadmapLabelModal(false)} />}
+          <ActionBtn className={`${classes.Btn} ${classes.RoadmapBtn}`} clicked={() => setShownModal('roadmap')}>{roadmapIcon}Roadmap Label</ActionBtn>
+          {shownModal === 'roadmap' && <RoadmapLabelModal close={closeHandler} />}
         </span>
       </span>
       <div className={classes.Title2}>ACTIONS</div>
       <span className={classes.BtnContainer2}>
-        <ActionBtn className={classes.Btn} clicked={() => setShowManualMoveModal(true)}>{arrowIcon} Move</ActionBtn>
-        <ActionBtn className={classes.Btn} clicked={() => setShowCopyModal(true)}>{copyIcon} Copy</ActionBtn>
+        <ActionBtn className={classes.Btn} clicked={() => setShownModal('move')}>{arrowIcon} Move</ActionBtn>
+        <ActionBtn className={classes.Btn} clicked={() => setShownModal('copy')}>{copyIcon} Copy</ActionBtn>
         <ActionBtn className={classes.Btn} clicked={props.archiveCard}>{archiveIcon} Archive</ActionBtn>
       </span>
-      {showCopyModal && <CopyCardModal close={() => setShowCopyModal(false)} />}
-      {showDueDateModal && <DueDateModal close={() => setShowDueDateModal(false)} />}
-      {showManualMoveModal && <ManualMoveModal close={() => setShowManualMoveModal(false)} />}
+      {shownModal === 'copy' && <CopyCardModal close={closeHandler} />}
+      {shownModal === 'date' && <DueDateModal close={closeHandler} />}
+      {shownModal === 'move' && <ManualMoveModal close={closeHandler} />}
     </div>
   );
 };
