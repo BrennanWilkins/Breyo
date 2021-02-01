4110bf441c09bdebf33ea0cd076a85f889aab39d
Brennan Wilkins <bwilkins767@gmail.com>
2021-02-01 12:25:50 -0600

    custom field icon util

---
+++ client/src/utils/customFieldUtils.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { textFieldIcon, numberFieldIcon, dateFieldIcon, checkboxFieldIcon } from '../components/UI/icons';
+
+export const fieldIcons = {
+  'Text': textFieldIcon,
+  'Number': numberFieldIcon,
+  'Checkbox': checkboxFieldIcon,
+  'Date': dateFieldIcon
+};
