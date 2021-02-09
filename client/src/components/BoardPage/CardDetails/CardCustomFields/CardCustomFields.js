42289626b093eb1f1f73dec8ddbdf7086034a531
Brennan Wilkins <bwilkins767@gmail.com>
2021-02-08 21:27:06 -0600

    Added custom fields to card details

---
+++ client/src/components/BoardPage/CardDetails/CardCustomFields/CardCustomFields.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import classes from './CardCustomFields.module.css';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { updateCustomFieldValue } from '../../../../store/actions';
+import { customFieldIcon } from '../../../UI/icons';
+import { fieldIcons } from '../../../../utils/customFieldUtils';
+// import TextCustomField from './TextCustomField';
+// import CheckboxCustomField from './CheckboxCustomField';
+// import DateCustomField from './DateCustomField';
+
+const CardCustomFields = props => (
+  <div>
+    <div className={classes.Title}>{customFieldIcon}Custom Fields</div>
+    <div className={classes.Fields}>
+      {props.customFields.map(({ fieldID, fieldTitle, fieldType, value }) => (
+        <div key={fieldID} className={classes.Field}>
+          <div className={`${classes.FieldTitle} ${fieldType === 'Date' ? classes.DateTitle : ''}`}>
+            {fieldIcons[fieldType]}
+            <span className={classes.TitleText}>{fieldTitle}</span>
+          </div>
+          {/*{
+            fieldType === 'Text' ? <TextCustomField value={value} updateValue={val => props.updateValue(fieldID, val)} /> :
+            fieldType === 'Number' ? <TextCustomField isNumber value={value} updateValue={val => props.updateValue(fieldID, val)} /> :
+            fieldType === 'Checkbox' ? <CheckboxCustomField value={value} updateValue={val => props.updateValue(fieldID, val)} /> :
+            <DateCustomField value={value} updateValue={val => props.updateValue(fieldID, val)} />
+          }*/}
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
+CardCustomFields.propTypes = {
+  customFields: PropTypes.array.isRequired,
+  updateValue: PropTypes.func.isRequired
+};
+
+const mapDispatchToProps = dispatch => ({
+  updateValue: (fieldID, value) => dispatch(updateCustomFieldValue(fieldID, value))
+});
+
+export default connect(null, mapDispatchToProps)(CardCustomFields);
--- client/src/components/BoardPage/CardDetails/CardDetails/CardDetails.js
+++ client/src/components/BoardPage/CardDetails/CardDetails/CardDetails.js
@@ -14,6 +14,7 @@ import CardChecklist from '../CardChecklist/CardChecklist';
 import { archiveCard } from '../../../../store/actions';
 import { alertIcon } from '../../../UI/icons';
 import CardMembers from '../CardMembers/CardMembers';
+import CardCustomFields from '../CardCustomFields/CardCustomFields';
 
 const CardDetails = props => {
   const modalRef = useRef();
@@ -48,6 +49,7 @@ const CardDetails = props => {
                 <CardDueDate currentDueDate={props.currentCard.dueDate} />}
             </div>
             <CardDesc currentDesc={props.currentCard.desc} />
+            {props.currentCard.customFields.length > 0 && <CardCustomFields customFields={props.currentCard.customFields} />}
             {props.currentCard.checklists.map(checklist => (
               <CardChecklist key={checklist.checklistID} {...checklist} />
             ))}
