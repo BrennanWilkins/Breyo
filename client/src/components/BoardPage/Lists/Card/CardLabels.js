7c51ff595f0d17fe6fc2afbad65d50a8cc6240d6
Brennan Wilkins <bwilkins767@gmail.com>
2021-02-28 10:44:32 -0600

    Card front label titles & animations

--- client/src/components/BoardPage/Lists/Card/CardLabels.js
+++ client/src/components/BoardPage/Lists/Card/CardLabels.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Card.module.css';
 import PropTypes from 'prop-types';
 import { LABEL_COLORS } from '../../../../utils/backgrounds';
 import { connect } from 'react-redux';
 
 const CardLabels = props => {
+  const [labelsHovered, setLabelsHovered] = useState(false);
+  const [expandLabels, setExpandLabels] = useState(false);
+
+  const clickHandler = e => {
+    e.stopPropagation();
+    setExpandLabels(prev => !prev);
+  };
+
   return (
     <div className={classes.Labels}>
-      {props.customLabels.map(labelID => <div key={labelID} style={{ background: props.customLabelsByID[labelID].color }} />)}
-      {LABEL_COLORS.filter(color => props.labels.includes(color)).map(color => <div key={color} style={{ background: color }} />)}
+      {props.customLabels.map(labelID => (
+        <div key={labelID} className={`${classes.Label} ${labelsHovered ? classes.DarkenLabel : ''} ${expandLabels ? classes.ExpandLabel : ''}`}
+        onClick={clickHandler} onMouseEnter={() => setLabelsHovered(true)} onMouseLeave={() => setLabelsHovered(false)}
+        style={{ background: props.customLabelsByID[labelID].color }}>
+          <div className={classes.LabelTitle}><span>{props.customLabelsByID[labelID].title}</span></div>
+        </div>
+      ))}
+      {LABEL_COLORS.filter(color => props.labels.includes(color)).map(color => (
+        <div key={color} className={`${classes.Label} ${labelsHovered ? classes.DarkenLabel : ''} ${expandLabels ? classes.ExpandLabel : ''}`}
+        onClick={clickHandler} onMouseEnter={() => setLabelsHovered(true)} onMouseLeave={() => setLabelsHovered(false)}
+        style={{ background: color }}>
+          <div className={classes.LabelTitle} />
+        </div>
+      ))}
     </div>
   );
 };
