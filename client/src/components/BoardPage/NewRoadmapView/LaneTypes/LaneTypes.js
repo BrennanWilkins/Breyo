ddef5ce88435c22ed62b21d48aecc96d61e59595
Brennan Wilkins <bwilkins767@gmail.com>
2021-03-09 22:24:52 -0600

    Unassigned cards roadmap modal

--- client/src/components/BoardPage/NewRoadmapView/LaneTypes/LaneTypes.js
+++ client/src/components/BoardPage/NewRoadmapView/LaneTypes/LaneTypes.js
@@ -1,44 +1,69 @@
-import React from 'react';
+import React, { useState, useRef, useLayoutEffect } from 'react';
 import classes from './LaneTypes.module.css';
 import PropTypes from 'prop-types';
 import { AccountBtn } from '../../../UI/Buttons/Buttons';
 import { plusIcon } from '../../../UI/icons';
+import UnassignedModal from '../UnassignedModal/UnassignedModal';
 
-const LaneTypes = props => (
-  <div className={classes.Container} style={{ minHeight: props.totalHeight }}>
-    {
-      props.mode === 'List' ?
-        props.lanes.map(({ id, height, title, unassignedCards }, i) => (
-          <div key={id} style={{ height }} className={classes.Lane}>
-            <div className={classes.Title}>
-              <div className={classes.TitleText}>{title}</div>
+const LaneTypes = props => {
+  const [shownModalTitle, setShownModalTitle] = useState('');
+  const [shownCards, setShownCards] = useState([]);
+  const [modalStyle, setModalStyle] = useState({ top: '0', left: '0' });
+  const containerRef = useRef();
+
+  useLayoutEffect(() => {
+    const rect = containerRef.current.getBoundingClientRect();
+    setModalStyle({ top: rect.top + 10 + 'px', left: rect.right + 10 + 'px' });
+  }, [shownModalTitle]);
+
+  const showModalHandler = (title, cards) => {
+    setShownModalTitle(title);
+    setShownCards(cards);
+  };
+
+  return (
+    <div className={classes.Container} style={{ minHeight: props.totalHeight }} ref={containerRef}>
+      {
+        props.mode === 'List' ?
+          props.lanes.map(({ id, height, title, unassignedCards }, i) => (
+            <div key={id} style={{ height }} className={classes.Lane}>
+              <div className={classes.Title}>
+                <div className={classes.TitleText}>{title}</div>
+              </div>
+              {unassignedCards.length > 0 && <div className={classes.UnassignedBtn} onClick={() => showModalHandler(title, unassignedCards)}>
+                ({unassignedCards.length}) Not Scheduled{plusIcon}
+              </div>}
             </div>
-            {unassignedCards.length > 0 && <div className={classes.UnassignedBtn}>({unassignedCards.length}) Not Scheduled{plusIcon}</div>}
-          </div>
-        ))
-      : props.mode === 'Member' ?
-        props.lanes.map(({ id, height, fullName, avatar, unassignedCards }) => (
-          <div key={id} style={{ height }} className={classes.Lane}>
-            <div className={classes.Title}>
-              <AccountBtn className={classes.Avatar} avatar={avatar}>{fullName ? fullName[0] : ''}</AccountBtn>
-              <div className={classes.TitleText}>{fullName}</div>
+          ))
+        : props.mode === 'Member' ?
+          props.lanes.map(({ id, height, fullName, avatar, unassignedCards }) => (
+            <div key={id} style={{ height }} className={classes.Lane}>
+              <div className={classes.Title}>
+                <AccountBtn className={classes.Avatar} avatar={avatar}>{fullName ? fullName[0] : ''}</AccountBtn>
+                <div className={classes.TitleText}>{fullName}</div>
+              </div>
+              {unassignedCards.length > 0 && <div className={classes.UnassignedBtn} onClick={() => showModalHandler(fullName, unassignedCards)}>
+                ({unassignedCards.length}) Not Scheduled{plusIcon}
+              </div>}
             </div>
-            {unassignedCards.length > 0 && <div className={classes.UnassignedBtn}>({unassignedCards.length}) Not Scheduled{plusIcon}</div>}
-          </div>
-        ))
-      :
-        props.lanes.map(({ id, height, color, title, unassignedCards }) => (
-          <div key={id} style={{ height }} className={classes.Lane}>
-            <div className={classes.Title}>
-              <div className={classes.Label} style={{ background: color || null }} />
-              <div className={classes.TitleText}>{title}</div>
+          ))
+        :
+          props.lanes.map(({ id, height, color, title, unassignedCards }) => (
+            <div key={id} style={{ height }} className={classes.Lane}>
+              <div className={classes.Title}>
+                <div className={classes.Label} style={{ background: color || null }} />
+                <div className={classes.TitleText}>{title}</div>
+              </div>
+              {unassignedCards.length > 0 && <div className={classes.UnassignedBtn} onClick={() => showModalHandler(title, unassignedCards)}>
+                ({unassignedCards.length}) Not Scheduled{plusIcon}
+              </div>}
             </div>
-            {unassignedCards.length > 0 && <div className={classes.UnassignedBtn}>({unassignedCards.length}) Not Scheduled{plusIcon}</div>}
-          </div>
-        ))
-    }
-  </div>
-);
+          ))
+      }
+      {shownModalTitle.length > 0 && <UnassignedModal style={modalStyle} close={() => setShownModalTitle('')} title={shownModalTitle} cards={shownCards} />}
+    </div>
+  );
+};
 
 LaneTypes.propTypes = {
   mode: PropTypes.string.isRequired,
---
+++ client/src/components/BoardPage/NewRoadmapView/UnassignedModal/UnassignedModal.js
@@ -0,0 +1,37 @@
+import React, { useRef } from 'react';
+import classes from './UnassignedModal.module.css';
+import PropTypes from 'prop-types';
+import { useModalToggle } from '../../../../utils/customHooks';
+import { CloseBtn } from '../../../UI/Buttons/Buttons';
+import RoadmapCard from '../RoadmapCard/RoadmapCard';
+
+const UnassignedModal = props => {
+  const modalRef = useRef();
+  useModalToggle(true, modalRef, props.close);
+
+  return (
+    <div ref={modalRef} style={props.style} className={classes.Container}>
+      <div className={classes.Title}>{props.title}</div>
+      <CloseBtn className={classes.CloseBtn} close={props.close} />
+      <div className={classes.Info}>Add a start and due date to schedule a card.</div>
+      <div className={classes.Cards}>
+        {props.cards.map(card => (
+          <RoadmapCard key={card.cardID} className={classes.Card} cardID={card.cardID}
+          listID={card.listID} members={card.members} title={card.title} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+UnassignedModal.propTypes = {
+  title: PropTypes.string.isRequired,
+  cards: PropTypes.array.isRequired,
+  close: PropTypes.func.isRequired,
+  style: PropTypes.shape({
+    top: PropTypes.string.isRequired,
+    left: PropTypes.string.isRequired
+  })
+};
+
+export default UnassignedModal;
